17b2ee2ea5456d4e279694678321ea1be579b590
wxibm <42483360+wxibm@users.noreply.github.com>
2025-05-06 22:09:04 +0800

    Add files via upload

---
+++ 项目6用户管理前端实现/6.2Vue调用接口/src/api/common.js
@@ -0,0 +1,16 @@
+let hostName = "http://127.0.0.1:8082/api"; //接口公共的ip或者域名
+
+//接口
+const apiUrls = {
+	userList: hostName + '/users/list', //获取所有用户数据
+	userPage: hostName + '/users/page', //获取分页用户数据
+	userCount: hostName + '/users/count', //获取用户记录总数
+	userRead: hostName + '/users/', //获取用户所有数据
+	userInsert: hostName + '/user', //添加用户
+	userUpdate: hostName + '/user', //修改用户
+	userDelete: hostName + '/user', //删除用户
+	userLogin: hostName + '/login', //用户登录
+	userNews: hostName + '/news/userId', //获取用户新闻
+};
+
+export default apiUrls
---
+++ 项目6用户管理前端实现/6.2Vue调用接口/src/main.js
@@ -0,0 +1,8 @@
+import {
+	createApp
+} from 'vue'
+import App from './App.vue'
+// 下面这句为新增的，所以需要use(router)
+import router from './router'
+createApp(App).use(router).mount('#app')
+// createApp(App).mount('#app')
